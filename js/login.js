8920749dcaf3553252c2e3c3e6e9cde3996847d6
NSAN80 <jeffrey.miller@ainsan80.com>
2026-03-18 18:20:14 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -88,14 +88,3 @@ clearButton.addEventListener("click", function () {
   emailInput.focus();
 });
 
-const toggleHint = document.getElementById("toggleHint");
-const hintBox = document.querySelector('[data-testid="login-hint"]');
-
-toggleHint.addEventListener("click", function () {
-  hintBox.classList.toggle("hidden");
-
-  toggleHint.textContent =
-    hintBox.classList.contains("hidden")
-      ? "Show QA Credentials"
-      : "Hide QA Credentials";
-});
